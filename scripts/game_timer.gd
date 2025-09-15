e5abee40150557f95995211b1b21ffdda5129283
Patrick Ly <patrickly7@yahoo.com>
2025-09-15 17:06:44 -0400

    Rotate timer hand with tween based on seconds elapsed and total time

--- scripts/game_timer.gd
+++ scripts/game_timer.gd
@@ -1,21 +1,39 @@
 extends Control
 
+var totalTime = 0
+var currentRotation = 0
+var degreesToAdd = 0
+
 var timer = 0
 signal game_timer_end(secondsLeft: int)
 
 func startTimer(seconds: int):
+	totalTime = seconds
+	degreesToAdd = 360 / totalTime
 	timer = seconds
 	print("Timer Started with %s" % str(timer))
+	
 	$Timer.start()
+	rotateTimerHand()
 
 func stopTimer(seconds: int):
 	$Timer.stop()
 	print("Timer Stopped at %s" % str(timer))
 	game_timer_end.emit(timer)
 
+func rotateTimerHand():
+	if (currentRotation == 360):
+		currentRotation = 0
+	
+	var tween = create_tween()
+	tween.tween_property($RotationPoint, "rotation_degrees", currentRotation + degreesToAdd, 1)
+	
+	currentRotation += degreesToAdd
+
 func _on_timer_timeout():
 	timer -= 1
 	print("Time Remaining: %s" % str(timer))
+	rotateTimerHand()
 	
 	if (timer <= 0):
 		$Timer.stop()
