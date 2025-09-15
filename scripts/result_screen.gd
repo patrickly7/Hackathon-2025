3d8ec7ccf41c8a1bb1fccd37858eb0e8bdb5467e
Patrick Ly <patrickly7@yahoo.com>
2025-09-15 15:33:31 -0400

    Added individual game results screen and global vars for it

--- scripts/result_screen.gd
+++ scripts/result_screen.gd
@@ -1,5 +1,61 @@
 extends Control
 
+var timeTaken = 0
+var mistakes = 0
+var medal = "Gold"
+
+func _ready():
+	$StationTitle.text = Global.CURRENT_GAME
+	getCurrentGameResults()
+	
+	showTimeTakenText()
+	showMistakesText()
+	showMedal()
+
+func getCurrentGameResults():
+	match Global.CURRENT_GAME:
+		"Dock Receive":
+			timeTaken = Global.DOCK_RECEIVE_TIME_TAKEN
+			mistakes = Global.DOCK_RECEIVE_MISTAKES
+			medal = Global.DOCK_RECEIVE_MEDAL
+		"Confirm Receive":
+			timeTaken = Global.CONFIRM_RECEIVE_TIME_TAKEN
+			mistakes = Global.CONFIRM_RECEIVE_MISTAKES
+			medal = Global.CONFIRM_RECEIVE_MEDAL
+		"Inventory":
+			timeTaken = Global.INVENTORY_TIME_TAKEN
+			mistakes = Global.INVENTORY_MISTAKES
+			medal = Global.INVENTORY_MEDAL
+		"Picking":
+			timeTaken = Global.PICKING_TIME_TAKEN
+			mistakes = Global.PICKING_MISTAKES
+			medal = Global.PICKING_MEDAL
+		"Packing":
+			timeTaken = Global.PACKING_TIME_TAKEN
+			mistakes = Global.PACKING_MISTAKES
+			medal = Global.PACKING_MEDAL
+		"Shipping":
+			timeTaken = Global.SHIPPING_TIME_TAKEN
+			mistakes = Global.SHIPPING_MISTAKES
+			medal = Global.SHIPPING_MEDAL
+		_:
+			pass
+
+func showTimeTakenText():
+	$TimeTakenValue.text = str(timeTaken)
+	$TimeTakenValue.show()
+	
+func showMistakesText():
+	$MistakesValue.text = str(mistakes)
+	$MistakesValue.show()
+	
+func showMedal():
+	if medal == "Gold":
+		$GoodJobText.show()
+	elif medal == "Silver":
+		$OkayJobText.show()
+	else:
+		$BadJobText.show()
 
 func _on_continue_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game_tracker.tscn")
