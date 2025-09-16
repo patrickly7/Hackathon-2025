ad7131deb61d5dca2ce937d5cbc8682d34d43616
Patrick Ly <patrickly7@yahoo.com>
2025-09-16 13:10:03 -0400

    fix dock receive scoring

--- scripts/dock_receive.gd
+++ scripts/dock_receive.gd
@@ -10,7 +10,13 @@ var startingPos = Vector2.ZERO
 var isTouchingBox = false
 var currentBox: Area2D
 
+const BOXES_TO_BOOP = 15
+var currentBoxesBooped = 0
+
 func _ready():	
+	# Reset Mistakes from any previous run
+	Global.DOCK_RECEIVE_MISTAKES = 0
+	
 	startingPos = $PlayerBody.position
 	$GameTimer.startTimer(GAME_TIME)
 	$BGMusic.play()
@@ -27,8 +33,21 @@ func _input(event):
 		$PlayerBody/Player.texture = FRONT_SPRITE
 	elif event.is_action_pressed("action") && isTouchingBox && currentBox != null:
 		currentBox.queue_free()
+		currentBoxesBooped += 1
+		if (currentBoxesBooped == BOXES_TO_BOOP):
+			$GameTimer.stopTimer()
 
 func _on_game_timer_game_timer_end(secondsLeft: int):
+	Global.DOCK_RECEIVE_TIME_TAKEN = GAME_TIME - secondsLeft
+	# Note: Mistakes are set by the box destruction
+	
+	if (secondsLeft > 0 && Global.DOCK_RECEIVE_MISTAKES == 0):
+		Global.DOCK_RECEIVE_MEDAL = "Gold"
+	elif (secondsLeft > 0 && Global.DOCK_RECEIVE_MISTAKES <= 5):
+		Global.DOCK_RECEIVE_MEDAL = "Silver"
+	else:
+		Global.DOCK_RECEIVE_MEDAL = "Bronze"
+	
 	Global.NEXT_GAME_SCENE = "res://scenes/confirm_receive.tscn"
 	get_tree().change_scene_to_file("res://scenes/result_screen.tscn")
 
