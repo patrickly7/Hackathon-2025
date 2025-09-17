c4c2498a31faece936541433a266b059523188cc
Patrick Ly <patrickly7@yahoo.com>
2025-09-17 17:54:53 -0400

    Added game icons to game tracker

--- scripts/game_tracker.gd
+++ scripts/game_tracker.gd
@@ -7,11 +7,40 @@ func _ready():
 	
 	if (Global.CURRENT_GAME == "Dock Receive"):
 		$LastGameBubble.hide()
+		$LastGameBubbleSprite.hide()
 		$MiniBubblesLeft.hide()
 	elif (Global.CURRENT_GAME == "Shipping"):
 		$NextGameBubble.hide()
+		$NextGameBubbleSprite.hide()
 		$MiniBubblesRight.hide()
+		
+	setGameBubbleSprites()
 
 func _input(event):
 	if event.is_action_pressed("action"):
 		get_tree().change_scene_to_file(Global.NEXT_GAME_SCENE)
+
+func setGameBubbleSprites():
+	match Global.CURRENT_GAME:
+		"Dock Receive":
+			$CurrentGameBubbleSprite.texture = load("res://assets/art/Dock_Receive_Icon.png")
+			$NextGameBubbleSprite.texture = load("res://assets/art/Confirm_Receive_Icon.png")
+		"Confirm Receive":
+			$LastGameBubbleSprite.texture = load("res://assets/art/Dock_Receive_Icon.png")
+			$CurrentGameBubbleSprite.texture = load("res://assets/art/Confirm_Receive_Icon.png")
+			$NextGameBubbleSprite.texture = load("res://assets/art/Inventory_Icon.png")
+		"Inventory":
+			$LastGameBubbleSprite.texture = load("res://assets/art/Confirm_Receive_Icon.png")
+			$CurrentGameBubbleSprite.texture = load("res://assets/art/Inventory_Icon.png")
+			$NextGameBubbleSprite.texture = load("res://assets/art/Picking_Icon.png")
+		"Picking":
+			$LastGameBubbleSprite.texture = load("res://assets/art/Inventory_Icon.png")
+			$CurrentGameBubbleSprite.texture = load("res://assets/art/Picking_Icon.png")
+			$NextGameBubbleSprite.texture = load("res://assets/art/Packing_Icon.png")
+		"Packing":
+			$LastGameBubbleSprite.texture = load("res://assets/art/Picking_Icon.png")
+			$CurrentGameBubbleSprite.texture = load("res://assets/art/Packing_Icon.png")
+			$NextGameBubbleSprite.texture = load("res://assets/art/Shipping_Icon.png")
+		"Shipping":
+			$LastGameBubbleSprite.texture = load("res://assets/art/Packing_Icon.png")
+			$CurrentGameBubbleSprite.texture = load("res://assets/art/Shipping_Icon.png")
